284c53f08fe16a61508569e52e3ec733a8eac419
Raul Laprida <raul.laprida@gmail.com>
2019-11-26 12:14:15 -0300

    Send message interface changed

--- src/dht.js
+++ src/dht.js
@@ -1,60 +1,82 @@
-'use strict'
+"use strict";
 
-const nextTick = require('async/nextTick')
-const errCode = require('err-code')
-const promisify = require('promisify-es6')
+const nextTick = require("async/nextTick");
+const errCode = require("err-code");
+const promisify = require("promisify-es6");
 
-const { messages, codes } = require('./errors')
+const { messages, codes } = require("./errors");
 
-module.exports = (node) => {
+module.exports = node => {
   return {
     put: promisify((key, value, callback) => {
       if (!node._dht) {
-        return nextTick(callback, errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED))
+        return nextTick(
+          callback,
+          errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED)
+        );
       }
 
-      node._dht.put(key, value, callback)
+      node._dht.put(key, value, callback);
     }),
     get: promisify((key, options, callback) => {
-      if (typeof options === 'function') {
-        callback = options
-        options = {}
+      if (typeof options === "function") {
+        callback = options;
+        options = {};
       }
 
       if (!node._dht) {
-        return nextTick(callback, errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED))
+        return nextTick(
+          callback,
+          errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED)
+        );
       }
 
-      node._dht.get(key, options, callback)
+      node._dht.get(key, options, callback);
     }),
     getMany: promisify((key, nVals, options, callback) => {
-      if (typeof options === 'function') {
-        callback = options
-        options = {}
+      if (typeof options === "function") {
+        callback = options;
+        options = {};
       }
 
       if (!node._dht) {
-        return nextTick(callback, errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED))
+        return nextTick(
+          callback,
+          errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED)
+        );
       }
 
-      node._dht.getMany(key, nVals, options, callback)
+      node._dht.getMany(key, nVals, options, callback);
     }),
-    sendMessage: promisify((key, value, partialAddressing, callback) => {
 
-      if (!node._dht) {
-        return nextTick(callback, errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED))
+    sendMessage: promisify(
+      (userId, msgContent, msgNonce, partialAddressing, callback) => {
+        if (!node._dht) {
+          return nextTick(
+            callback,
+            errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED)
+          );
+        }
+
+        node._dht.sendMessage(
+          userId,
+          msgContent,
+          msgNonce,
+          partialAddressing,
+          callback
+        );
       }
-
-      node._dht.sendMessage(key, value, partialAddressing, callback)
-    }),
+    ),
     registerListener: promisify((eventId, listenerFunction, callback) => {
-
       if (!node._dht) {
-        return nextTick(callback, errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED))
+        return nextTick(
+          callback,
+          errCode(new Error(messages.DHT_DISABLED), codes.DHT_DISABLED)
+        );
       }
 
-      node._dht.on(eventId,listenerFunction)
-      callback()
+      node._dht.on(eventId, listenerFunction);
+      callback();
     })
-  }
-}
+  };
+};
